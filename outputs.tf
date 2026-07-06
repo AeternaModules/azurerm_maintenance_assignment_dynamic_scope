19d2cca0930e76206febc3c11c6046e802d2ae98
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "maintenance_assignment_dynamic_scopes" {
   description = "All maintenance_assignment_dynamic_scope resources"
   value       = azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes
 }
+output "maintenance_assignment_dynamic_scopes_filter" {
+  description = "List of filter values across all maintenance_assignment_dynamic_scopes"
+  value       = [for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : v.filter]
+}
+output "maintenance_assignment_dynamic_scopes_maintenance_configuration_id" {
+  description = "List of maintenance_configuration_id values across all maintenance_assignment_dynamic_scopes"
+  value       = [for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : v.maintenance_configuration_id]
+}
+output "maintenance_assignment_dynamic_scopes_name" {
+  description = "List of name values across all maintenance_assignment_dynamic_scopes"
+  value       = [for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : v.name]
+}
 
