11c68a0f37be339423992b692bf5bd44e637065a
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "maintenance_assignment_dynamic_scopes_id" {
   description = "Map of id values across all maintenance_assignment_dynamic_scopes, keyed the same as var.maintenance_assignment_dynamic_scopes"
-  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.id }
+  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "maintenance_assignment_dynamic_scopes_filter" {
   description = "Map of filter values across all maintenance_assignment_dynamic_scopes, keyed the same as var.maintenance_assignment_dynamic_scopes"
-  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.filter }
+  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.filter if v.filter != null && length(v.filter) > 0 }
 }
 output "maintenance_assignment_dynamic_scopes_maintenance_configuration_id" {
   description = "Map of maintenance_configuration_id values across all maintenance_assignment_dynamic_scopes, keyed the same as var.maintenance_assignment_dynamic_scopes"
-  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.maintenance_configuration_id }
+  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.maintenance_configuration_id if v.maintenance_configuration_id != null && length(v.maintenance_configuration_id) > 0 }
 }
 output "maintenance_assignment_dynamic_scopes_name" {
   description = "Map of name values across all maintenance_assignment_dynamic_scopes, keyed the same as var.maintenance_assignment_dynamic_scopes"
-  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.name }
+  value       = { for k, v in azurerm_maintenance_assignment_dynamic_scope.maintenance_assignment_dynamic_scopes : k => v.name if v.name != null && length(v.name) > 0 }
 }
 
